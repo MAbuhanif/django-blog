00eaccb95086bc0e6659c0b80115b0f49c1455ef
Mundino Hamure <mundinoabete@gmail.com>
2024-12-10 18:13:20 +0000

    Add meta class

--- blog/models.py
+++ blog/models.py
@@ -14,5 +14,26 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on", "author" ]
+
+    def __str__(self):
+        return f"{self.title} | Written by {self.author}"
+
+
+
 class Comment(models.Model):
-    title = models.CharField(max_length=200, unique=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author= models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    
+    
+
+    class Meta:
+        ordering= ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
+    
